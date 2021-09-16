900433c0c368697574708879f30744f6cc132877
UlissesSilva08 <ulissesmasterchief@gmail.com>
2021-09-16 20:54:16 -0300

    Inserindo a variavel double e a formula para calcular o °F

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GrausCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Conversor de temperaturas");
+
+            Console.WriteLine();
+
+            Console.Write("Digite os graus(°C): ");
+            string cValor = Console.ReadLine();
+            double cNumero = Int32.Parse(cValor);
+
+            double fValor = cNumero*1.8+32;
+
+            Console.WriteLine();
+
+            Console.WriteLine("°C = " + cNumero);
+            Console.WriteLine(cNumero + "°C é equivalente a " + fValor + "°F");
+        }
+    }
+}
